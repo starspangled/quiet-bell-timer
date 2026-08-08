9634d59fd356af8b33bee2db4edf54e6ee29c812
RJ <starspangled@users.noreply.github.com>
2026-08-08 11:40:18 -0700

    Add Quiet Bell Timer install support

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',event=>event.waitUntil(caches.open('quiet-bell-v1').then(cache=>cache.addAll(['./','./index.html','./styles.css','./timer.js','./audio.js','./app.js','./nature-bg.png','./manifest.webmanifest']))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
