40387cb34ce2debd05fb114591512f003854e595
RJ <starspangled@users.noreply.github.com>
2026-08-08 12:26:51 -0700

    Use working direct audio path for timer chimes

--- audio.js
+++ audio.js
@@ -4,7 +4,7 @@
     bell=new Audio('./bell.wav');
     bell.preload='auto';
     bell.setVolume=function(v){volume=Number(v);bell.volume=volume};
-    bell.playBell=function(){bell.pause();bell.currentTime=0;bell.volume=volume;const p=bell.play();if(p)p.catch(()=>{})};
+    bell.playBell=function(){const chime=new Audio('./bell.wav?v=3');chime.preload='auto';chime.volume=volume;const p=chime.play();if(p)p.catch(()=>{})};
     bell.setVolume(volume);
     return {setVolume:v=>bell.setVolume(v),play:()=>bell.playBell(),unlock:()=>{bell.muted=true;const p=bell.play();if(p)p.then(()=>{bell.pause();bell.currentTime=0;bell.muted=false}).catch(()=>{bell.muted=false})}};
   }
