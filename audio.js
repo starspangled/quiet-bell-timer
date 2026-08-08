78fa048020482811dfbf7d4e50a1f234ee5268a8
RJ <starspangled@users.noreply.github.com>
2026-08-08 12:30:45 -0700

    Reuse unlocked audio element for repeated iPhone chimes

--- audio.js
+++ audio.js
@@ -1,12 +1,14 @@
 (function(global){
-  let bell, volume=.55;
+  let volume=.55;
   function createBellAudio(){
-    bell=new Audio('./bell.wav');
+    const bell=new Audio('./bell.wav?v=4');
     bell.preload='auto';
-    bell.setVolume=function(v){volume=Number(v);bell.volume=volume};
-    bell.playBell=function(){const chime=new Audio('./bell.wav?v=3');chime.preload='auto';chime.volume=volume;const p=chime.play();if(p)p.catch(()=>{})};
-    bell.setVolume(volume);
-    return {setVolume:v=>bell.setVolume(v),play:()=>bell.playBell(),unlock:()=>{bell.muted=true;const p=bell.play();if(p)p.then(()=>{bell.pause();bell.currentTime=0;bell.muted=false}).catch(()=>{bell.muted=false})}};
+    const api={
+      setVolume(v){volume=Number(v);bell.volume=volume},
+      unlock(){bell.muted=true;bell.currentTime=0;const p=bell.play();if(p)p.then(()=>{bell.pause();bell.currentTime=0;bell.muted=false}).catch(()=>{bell.muted=false})},
+      play(){bell.muted=false;bell.volume=volume;bell.currentTime=0;const p=bell.play();if(p)p.catch(()=>{})}
+    };
+    api.setVolume(volume);global.bellController=api;return api;
   }
   global.createBellAudio=createBellAudio;
 })(window);
